0879afeda0b5945cd95d86c0e3cb91bde33f60e4
Wazarr94 <jeje_04@live.com>
2023-10-16 14:56:01 +0200

    change proxy for prod mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { config } from 'dotenv';
+
+config();
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		proxy: {
 			'/api': {
-				target: 'http://localhost:8000',
+				target:
+					process.env.PROD_ENV == 'development'
+						? 'http://localhost:8000'
+						: 'https://haxball-cheat-detector-production.up.railway.app',
+
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, '')
 			}
